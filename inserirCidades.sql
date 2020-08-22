716ef6c360d5ae2584d3c6c65d980f2a8820e7d1
aldo-pereira22 <aldopereira.gyn@gmail.com>
2020-08-22 10:50:58 -0300

    insert com select

--- inserirCidades.sql
+++ inserirCidades.sql
@@ -3,3 +3,12 @@ values ('Campinas', 795, 25)
 
 insert into cidades (nome, area, estado_id)
 values ('Caruaru', 920.6, ( select id from estados where sigla = 'PE' ))
+
+
+insert into cidades
+	(nome, area, estado_id)
+values (
+	'Juazeiro do norte',
+    248.2,
+    (select id from estados where sigla = 'CE' )
+    )
